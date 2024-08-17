6a8e3cd30061f9313c4defd50b5c0975ec51fbb2
j7d15 <j7devweb@gmail.com>
2024-08-17 17:44:46 +0200

    add ci

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,6 +23,7 @@ const config = {
   organizationName: 'j7d15', // Usually your GitHub org/user name.
   projectName: 'wiki', // Usually your repo name.
   deploymentBranch: 'gh-pages',
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
